d9910bbfa15cf3496fdb59cf3df12579c9acc84e
Masaniprakash M <91075656+Masaniprakash@users.noreply.github.com>
2025-09-06 21:52:14 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const db = require('./models/index'); 
+const session = require('express-session');
+var passport = require("passport");
+var authRoutes = require('./routes/user_data')
+var orgRoutes = require('./routes/organization')
+var inviteRoutes = require('./routes/invite')
+var cors = require('cors')
+
+const app = express();
+
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors())
+
+app.use(session({
+  secret: "SECRET_KEY",
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/organization", orgRoutes);
+app.use("/api/invite", inviteRoutes);
+
+app.get("/mass",(req,res)=>{
+  try {
+    let secrect = process.env.PASSWORD
+    res.send(secrect)
+  } catch (error) {
+    
+  }
+})
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`server listen on http://localhost:${port}`);
+});
+
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIxYzdlMGM0ZS05MDFlLTQ3YmMtYTkwMS1mYzMxMDRhODQ1NTciLCJpYXQiOjE3NTY5MDE5ODh9.bxIXCsq83idQlaOB4Ld-Vdycz9olv8cH36h1KRSOCB8
---
+++ src/routes/user_data.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var router = express.Router();
+const {sendOtpToEmail, verifyOtp, googleAuth} = require("../controllers/authController")
+
+router.post("/send/otp", sendOtpToEmail);
+router.post("/verify/otp", verifyOtp);
+router.get("/google", googleAuth);
+
+module.exports = router;
